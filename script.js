6b8ded1180dc8298518e5a94b8558d03c6db61be
Rohith-Daruri <139214300+Rohith-Daruri@users.noreply.github.com>
2024-01-31 08:26:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let e = document.addEventListener("DOMContentLoaded",function fun(){
+   let e = document.createElement("p");
+   e.innerText ="DOM load success";
+   document.body.append(e);
+})
